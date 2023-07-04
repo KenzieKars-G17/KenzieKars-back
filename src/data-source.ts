d046e6e979403a6a3b8248292956d855d6ee0754
cebarrico <cbarrico@gmail.com>
2023-07-04 11:30:29 -0300

    migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,41 +1,45 @@
-import 'dotenv/config'
-import 'reflect-metadata'
-import { DataSource, DataSourceOptions } from 'typeorm'
-import path from 'path'
+import "dotenv/config";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+import path from "path";
 
 const DataSourceConfig = (): DataSourceOptions => {
-    const entitiesPath: string = path.join(__dirname, './entities/**.{ts,js}')
-    const migrationsPath: string = path.join(__dirname, './migrations/**.{ts,js}')
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{ts,js}"
+  );
 
-    const dbUrl: string | undefined = process.env.DATABASE_URL
+  const dbUrl: string | undefined = process.env.DATABASE_URL;
 
-    if(!dbUrl){
-        throw new Error('Env var DATABASE_URL does not exists')
-    }
+  if (!dbUrl) {
+    throw new Error("Env var DATABASE_URL does not exists");
+  }
 
-    const nodeEnv: string | undefined = process.env.NODE_ENV
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
 
-    if(nodeEnv === 'test'){
-        return {
-            type: 'sqlite',
-            database: ':memory:',
-            synchronize: true,
-            entities: [entitiesPath]
-        }
-    }
-
+  if (nodeEnv === "test") {
     return {
-        type: 'postgres',
-        url: dbUrl,
-        synchronize: false,
-        logging: true,
-        migrations: [migrationsPath],
-        entities: [entitiesPath]
-    }
-}
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
 
-const AppDataSource = new DataSource(DataSourceConfig())
+  return {
+    type: "postgres",
+    url: dbUrl,
+    synchronize: false,
+    logging: true,
+    migrations: [migrationsPath],
+    entities: [entitiesPath],
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+};
 
-export {
-    AppDataSource
-}
+const AppDataSource = new DataSource(DataSourceConfig());
+
+export { AppDataSource };
