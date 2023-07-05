1764db65c096ea44bd7c479ba05191a831d544ab
luis-mendes-95 <luis.mendes.95@hotmail.com>
2023-07-05 17:09:01 -0300

    fix image limitation when adding varying number of images in advertisement

--- src/services/advertisements/createAdvertisements.service.ts
+++ src/services/advertisements/createAdvertisements.service.ts
@@ -10,45 +10,34 @@ import {
 } from "../../schemas/advertisement.schema";
 import createImagesAdvertisementService from "../images/createImage.service";
 
-const createAdvertisementService = async (  data: TAdvertisementReq,  userId: number, galleryImages: any): Promise<TAdvertisement> => {
-
-  console.log('***********************************************************************************************')
-  console.log('*BODY QUE VIROU DATA NO SERVICE**********************************************************************************************')
-  console.log(data)
-  console.log('***********************************************************************************************')
-  console.log('***********************************************************************************************')
-  console.log('***********************************************************************************************')
-  console.log('GALLERY IMAGES***********************************************************************************************')
-  console.log(galleryImages)
-  console.log('***********************************************************************************************')
-
+const createAdvertisementService = async (
+  data: TAdvertisementReq,
+  userId: number,
+  galleryImages: any
+): Promise<TAdvertisement> => {
   const usersRepository = AppDataSource.getRepository(User);
   const advertisementRepository = AppDataSource.getRepository(Advertisement);
 
-  const user: User | null = await usersRepository.findOneBy({    id: userId,  });
+  const user: User | null = await usersRepository.findOneBy({ id: userId });
 
-  if (!user) {    throw new AppError("User does not exists", 404);  }
+  if (!user) {
+    throw new AppError("User does not exists", 404);
+  }
 
-  const advertisement: Advertisement | null = advertisementRepository.create({    ...data,    user: user,  });
+  const advertisement: Advertisement | null = advertisementRepository.create({
+    ...data,
+    user: user,
+  });
 
   await advertisementRepository.save(advertisement);
 
-  if (galleryImages[0].image !== "") {
-    createImagesAdvertisementService(galleryImages[0], advertisement.id)
-  }
-  if (galleryImages[1].image !== "") {
-    createImagesAdvertisementService(galleryImages[1], advertisement.id)
-  }
-  if (galleryImages[2].image !== "") {
-    createImagesAdvertisementService(galleryImages[2], advertisement.id)
-  }
-  if (galleryImages[3].image !== "") {
-    createImagesAdvertisementService(galleryImages[3], advertisement.id)
+  for (let i = 0; i < galleryImages.length; i++) {
+    if (galleryImages[i].image !== "") {
+      createImagesAdvertisementService(galleryImages[i], advertisement.id);
+    }
   }
 
   return advertisementSchema.parse(advertisement);
-
-
 };
 
 export { createAdvertisementService };
